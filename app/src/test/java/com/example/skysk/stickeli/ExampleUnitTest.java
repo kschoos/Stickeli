63dd93c0df23f291b3ec96a267991af0c48ccc41
kschoos <kschoos@student.tugraz.at>
2018-07-14 13:11:09 +0200

    Added test forr merging maps

--- app/src/test/java/com/example/skysk/stickeli/ExampleUnitTest.java
+++ app/src/test/java/com/example/skysk/stickeli/ExampleUnitTest.java
@@ -2,6 +2,14 @@ package com.example.skysk.stickeli;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 /**
@@ -15,5 +23,33 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void gettingBarEntries_isCorrect() throws NoSuchMethodException {
+        // MonthlyStatsFragment msf = new MonthlyStatsFragment();
+        // Method getBarEntriesFromProject = MonthlyStatsFragment.class.getDeclaredMethod("GetBarEntriesFromProjects", List.class);
+        // getBarEntriesFromProject.setAccessible(true);
+
+        // ArrayList<Project> projects = new ArrayList<>();
+        // Project s1 = new Project("Stickeli1");
+
+        // getBarEntriesFromProject.invoke(msf, )
+
+        ProjectModel pm = ProjectModel.getInstance();
+        pm.AddProject("S1", 15, null);
+        pm.AddProject("S2", 70, null);
+
+        Map<String, Long> counts = new HashMap<>();
+
+        for(Project project: pm.mProjects)
+        {
+            project.mMonthlyHalfCrossCount.forEach((k, v) -> counts.merge(k, v, (first, second) -> first + second));
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+        String today = sdf.format(new Date());
+
+        assertEquals((long) counts.get(today), 170);
     }
 }
