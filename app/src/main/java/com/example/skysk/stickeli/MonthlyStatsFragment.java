e2480831c545ac2960e3a77f6cb4cbca0ec2b2ab
kschoos <kschoos@student.tugraz.at>
2018-07-14 13:07:43 +0200

    Created Placeholder DataSet and Bar showing up.

--- app/src/main/java/com/example/skysk/stickeli/MonthlyStatsFragment.java
+++ app/src/main/java/com/example/skysk/stickeli/MonthlyStatsFragment.java
@@ -1,72 +1,76 @@
 package com.example.skysk.stickeli;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MonthlyStatsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MonthlyStatsFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private BarChart mChart;
+    private ProjectModel mProjectModel;
 
     public MonthlyStatsFragment() {
         // Required empty public constructor
     }
 
+    private List<BarEntry> GetBarEntriesFromProjects(List<Project> tProjects)
+    {
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        Map<String, Long> counts = new HashMap<>();
+
+        for(Project project: tProjects)
+        {
+            project.mMonthlyHalfCrossCount.forEach((k, v) -> counts.merge(k, v, (first, second) -> first + second));
+        }
+
+        return barEntries;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mProjectModel = ProjectModel.getInstance();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_monthly_stats, container, false);
+        mChart =  view.findViewById(R.id.monthly_chart);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(
+                0,
+                new float[]{22.0f, 33.0f, 24.0f})
+        );
+
+        barEntries.add(new BarEntry(
+                1,
+                new float[]{22.0f, 33.0f})
+        );
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Monthly Stitch Stats");
+        BarData barData = new BarData(barDataSet);
+        mChart.setData(barData);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_monthly_stats, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return view;
     }
 }
