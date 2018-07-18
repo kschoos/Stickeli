b9b2bf4de5c12e371b16580711916da2d1720aa1
kschoos <kschoos@student.tugraz.at>
2018-07-18 18:15:04 +0200

    [Charts] Added DateAxisFormatter

---
+++ app/src/main/java/com/example/skysk/stickeli/DateAxisFormatter.java
@@ -0,0 +1,11 @@
+package com.example.skysk.stickeli;
+
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+/**
+ * Created by skysk on 7/18/2018.
+ */
+
+public class DateAxisFormatter implements IAxisValueFormatter {
+
+}
