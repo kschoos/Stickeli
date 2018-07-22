3b7c5d9ff6c689f3c877a90b768c03aa59fcf67a
kschoos <kschoos@student.tugraz.at>
2018-07-22 10:46:17 +0200

    [Charts] Implemented DateAxisFormatter

--- app/src/main/java/com/example/skysk/stickeli/DateAxisFormatter.java
+++ app/src/main/java/com/example/skysk/stickeli/DateAxisFormatter.java
@@ -1,6 +1,11 @@
 package com.example.skysk.stickeli;
 
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by skysk on 7/18/2018.
@@ -8,4 +13,15 @@ import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 public class DateAxisFormatter implements IAxisValueFormatter {
 
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        Calendar cal = Calendar.getInstance();
+        try {
+            cal.setTime(DateUtils.dateFormat.parse(String.format("%.0f", value)));
+            return DateUtils.humanDateFormat.format(cal.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
