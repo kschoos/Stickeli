ec4332aef4a9a5a2351bbd06b5cd8d2edfdc184b
kschoos <kschoos@student.tugraz.at>
2018-07-22 13:29:31 +0200

    [Charts] DailyPie showing up in colors!

--- app/src/main/java/com/example/skysk/stickeli/Fragments/SummaryStatsFragment.java
+++ app/src/main/java/com/example/skysk/stickeli/Fragments/SummaryStatsFragment.java
@@ -3,11 +3,14 @@ package com.example.skysk.stickeli.Fragments;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.skysk.stickeli.ChartHelper;
 import com.example.skysk.stickeli.Project;
+import com.example.skysk.stickeli.ProjectModel;
 import com.example.skysk.stickeli.R;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
@@ -22,6 +25,7 @@ import java.util.List;
  */
 public class SummaryStatsFragment extends Fragment {
 
+    ProjectModel mProjectModel;
     PieChart mMonthlyPie;
     PieChart mWeeklyPie;
     PieChart mDailyPie;
@@ -32,31 +36,42 @@ public class SummaryStatsFragment extends Fragment {
 
         for(Project project: pProjects)
         {
+            PieEntry entry = new PieEntry(project.GetDailyStitchCount(), project.mName);
+            entries.add(entry);
         }
 
-        //PieDataSet dailyDataSet = new PieDataSet();
-        return null;
+        PieDataSet dailyDataSet = new PieDataSet(entries, "Daily Stitch Count");
+
+        return dailyDataSet;
     }
 
     public SummaryStatsFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mProjectModel = ProjectModel.getInstance();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_summary_stats, container, false);
-        mMonthlyPie = getActivity().findViewById(R.id.monthly_pie);
-        mWeeklyPie = getActivity().findViewById(R.id.weekly_pie);
-        mDailyPie = getActivity().findViewById(R.id.daily_pie);
+        mMonthlyPie = view.findViewById(R.id.monthly_pie);
+        mWeeklyPie = view.findViewById(R.id.weekly_pie);
+        mDailyPie = view.findViewById(R.id.daily_pie);
 
-        PieDataSet pieDataSet = new PieDataSet()
+        int[] colors = ChartHelper.GetNColors(mProjectModel.mProjects.size(), getContext());
+        PieDataSet pieDataSet = GetDailySetFromProjects(mProjectModel.mProjects);
+        pieDataSet.setColors(colors);
 
-        //PieData dailyData = new Pie
+        PieData dailyData = new PieData(pieDataSet);
 
-        mDailyPie.setData()
+
+        mDailyPie.setData(dailyData);
 
         return view;
     }
